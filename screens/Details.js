4a30d994b71fda14ce3f61836ef1e7c49bba7a11
Taylan Kose <taylancankose@gmail.com>
2022-10-30 22:19:26 +0300

    nft project start

---
+++ screens/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+function Details() {
+  return (
+    <SafeAreaView>
+      <Text>Details</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Details;
